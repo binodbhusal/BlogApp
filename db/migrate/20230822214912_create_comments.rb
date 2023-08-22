1c3f8b92c210f111c6381a0d9e4be1dd4f259106
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-22 22:57:08 +0100

    include create comments

---
+++ db/migrate/20230822214912_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :author, foreign_key:{to_table: :users}, null:false, index:true
+      t.references :post, foreign_key: {to_table: :posts}, null:false, index:true
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
+
