6f12a8a5d4e1a58129ee847aab3aef2d2db19af4
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-23 22:41:55 +0100

    update files

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: :'User'
-    belongs_to :post, counter_cache: true
+  belongs_to :author, class_name: :User
+  belongs_to :post, counter_cache: true
 
-    after_create :update_likes_counter
-    after_distroy :update_likes_counter
-    def update_likes_counter
-        self.post.update(likes_counter: self.post.likes.count)
-    end
+  after_create :update_likes_counter
+  after_distroy :update_likes_counter
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
