989a518ef9d4fc6c6dd6c0471d6ed49d3ce163f2
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-29 15:33:30 +0100

    Add users controller spec

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+    it 'includes placeholder text in the responce body ' do
+      get users_path
+      expect(response.body).to include('here is a list of users')
+    end
+  end
+  describe 'GET #show' do
+    let(:user) { create(:user) }
+    it 'returns the successful response' do
+      get users_path(user)
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns the show template' do
+      user = create(:user)
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+    it 'includes placeholder text in the response body' do
+      user = create(:user, name: 'Tom')
+      get user_path(user)
+      expect(response.body).to include("user details of #{user.name}")
+    end
+  end
+end
