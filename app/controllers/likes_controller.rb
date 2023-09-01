844172effea2cb162a35be63dbe4c3e2b8f87fc9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-02 00:14:21 +0100

    Add likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new(author: current_user, post: @post)
+    if @like.save
+      flash[:success] = 'you liked this post'
+    else
+      flash[:error] = 'Falied to like the post'
+    end
+    redirect_to @post
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @post = @like.post
+    @like.destroy
+    flash[:success] = 'You unliked this post'
+    redirect_to @post
+  end
+end
