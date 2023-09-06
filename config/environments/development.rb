6f92d4ced9181cf21648111f00cd3730a636eac1
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-06 23:15:28 +0100

    Update schema.rb and development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+    # Set the default URL options for Devise in the development environment
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+}
 end
