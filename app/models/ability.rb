87e41226608fb97450e5ae8a39380ab6260eee28
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-08 12:26:52 +0100

    Update files

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,15 @@
-class Ability 
-    include CanCan::Ability
-  
-    def initialize(user) 
-      user ||= User.new
-      puts "User role: #{user.role}"
-      if user.admin?
-        can :manage, :all
-      else 
-        can :read, :all
-        can :destroy, Post, author_id: user.id
-        can :manage, Comment, author_id: user.id
-      end
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Post, author_id: user.id
+      can :destroy, Comment, author_id: user.id
+      can :manage, Comment, post: { author_id: user.id }
     end
   end
-  
+end
