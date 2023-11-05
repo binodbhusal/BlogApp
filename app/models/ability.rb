e4ad49d51284b766dbdfc062d58ba82efbade13d
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-05 20:24:37 +0000

    changes made in ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,16 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new
-    if user.admin?
+  def initialize(bloguser)
+    bloguser ||= Bloguser.new
+
+    if bloguser.admin?
       can :manage, :all
     else
       can :read, :all
-      can :manage, Post, author_id: user.id
-      can :destroy, Comment, author_id: user.id
-      can :manage, Comment, post: { author_id: user.id }
+      can :manage, Post, author_id: bloguser.id
+      can :destroy, Comment, author_id: bloguser.id
+      can :manage, Comment, post: { author_id: bloguser.id }
     end
   end
 end
