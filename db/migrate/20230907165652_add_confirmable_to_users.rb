7dc9fe60e7f074d077df427d627a1ef36f661449
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-05 20:27:57 +0000

    Update migrations

--- db/migrate/20230907165652_add_confirmable_to_users.rb
+++ db/migrate/20230907165652_add_confirmable_to_users.rb
@@ -1,8 +1,8 @@
 class AddConfirmableToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmation_sent_at, :datetime
-    add_column :users, :unconfirmed_email, :string
+    add_column :blog_users, :confirmed_at, :datetime
+    add_column :blog_users, :confirmation_token, :string
+    add_column :blog_users, :confirmation_sent_at, :datetime
+    add_column :blog_users, :unconfirmed_email, :string
   end
 end
